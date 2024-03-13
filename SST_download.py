4f883b2ae24442e286bdacdc0219876f298bed8b
Leo Bertini <leonardo.bertini25@gmail.com>
2024-03-13 23:53:04 +0000

    Updates

--- SST_download.py
+++ SST_download.py
@@ -10,7 +10,9 @@ import nctoolkit as nc
 import pandas as pd
 from scipy.stats import pearsonr
 import seaborn as sns
-
+from haversine import haversine
+from scipy.spatial import KDTree
+import geopy.distance
 
 def fix_calendar(ds, timevar='T'):
     if ds[timevar].attrs['calendar'] == '360':
@@ -18,21 +20,86 @@ def fix_calendar(ds, timevar='T'):
     return ds
 
 
-def extract_ERSSTv5(ncfille, Dataframe, TimeRangeCol, LatVarName, LonVarName, SlideLatVal_Up, SlideLonVal_Up,
+def extract_ERSSTv5(ncfile, Dataframe, TimeRangeCol, LatVarName, LonVarName, SlideLatVal_Up, SlideLonVal_Up,
                     SlideLatVal_Btm, SlideLonVal_Btm):
+
     for i in range(0, len(Dataframe)):
+
+       ##todo change to function
+       print(f"processing data entry row no. : {Dataframe['index'][i]}")
+
+       ilat= Dataframe['Lat'][i]
+       ilon = Dataframe['Lon'][i]+180 #because longitude is gridded 0-360,
+
+       nclats=np.asarray(ncfile.variables['lat'][:])
+       nclons=np.asarray(ncfile.variables['lon'][:])
+
+       time_start = (Dataframe[TimeRangeCol][i]).split('-')[0] + '-01-01T00:00'
+       time_end = (Dataframe[TimeRangeCol][i]).split('-')[1] + '-12-01T00:00'
+
+       # if data point is too old for series. adopt earliest 5 years of ERSST record
+       if int(Dataframe[TimeRangeCol][i].split('-')[0]) <= 1854:
+           time_start = '1854-01-01T00:00'
+           time_end = '1860-12-01T00:00'
+
+        #calculating closest grid point to target location
+        grid_point=[]
+        d = []
+        grid_point_idx = []
+        for lat_index in range(0,len(nclats)):
+            for lon_index in range(0, len(nclons)):
+                grid_point.append((nclats[lat_index],nclons[lon_index]))
+                grid_point_idx.append((lat_index,lon_index))
+                d.append(geopy.distance.geodesic((ilat,ilon), (nclats[lat_index], nclons[lon_index])).km)
+
+        out=list(zip(d,grid_point, grid_point_idx)) #zipped tuple of ((distance), (latgrid, longrid), (latgrid_idx, longrid_idx) )
+        out.sort(key=lambda x: x[0]) #sorting so first is the closest point relative to my target point
+
+        #now find valid grid cell with value closest to target location iterating over tuple of point distances
+        for close_point_idx in range(0, len(out)):
+            #slice data
+            check_series = ncfile.sel(lat=out[close_point_idx][1][0],
+                                      lon=out[close_point_idx][1][1],
+                                      time=slice(time_start, time_end))
+
+            if np.isnan(np.nanmean(check_series.variables['sst'][:,:])): #get average temperature ignoring if there's nan in the series and so return true if there's at least one valid value
+                print("Empty grid cell: Finding next valid point")
+
+            else:
+                print(f"found closest grid point with valid value {(out[close_point_idx][1][0],out[close_point_idx][1][1]-180) }")
+                print(f"original grid point is {ilat,ilon-180}")
+
+                values = np.asarray(check_series.variables['sst'][:,:])
+
+                # update values on dataframe
+                Dataframe.loc[i, "SST_ERSSTv5_sd"] = np.std(values)
+                Dataframe.loc[i, "SST_ERSSTv5_ann"] = np.mean(values)
+                Dataframe.loc[i, "SST_ERSSTv5_range"] = values.max()-values.min()
+                #todo get column for min
+                #todo get column for max
+                #todo get column for closes valid grid point coordinates
+                break
+       #######
+
+
+
+
+
+
 
         latrange = (round(Dataframe[LatVarName][i] + SlideLatVal_Up),
                     round(Dataframe[LatVarName][i] + SlideLatVal_Btm))
 
-        longrange = (round(Dataframe[LonVarName][i] + SlideLonVal_Up)+180, #because longitude in the model is gridded 0-360,
+        longrange = (round(Dataframe[LonVarName][i] + SlideLonVal_Up)+180,
                      round(Dataframe[LonVarName][i] + SlideLonVal_Btm)+180)
 
         time_start = (Dataframe[TimeRangeCol][i]).split('-')[0] + '-01-01T00:00'
         time_end = (Dataframe[TimeRangeCol][i]).split('-')[1] + '-12-01T00:00'
 
         dummy = ncfille.sel(lon=slice(longrange[0], longrange[1]), lat=slice(latrange[0], latrange[1]),
-                            time=slice(time_start, time_end))
+                            time=slice(time_start, time_end), method='nearest')
+
+
 
         # for regions with NaN SST and between 1800-1850, get the 1854-1854 value to then subtract from anomalies based on proxy studies
         if np.isnan(float(dummy.variables['sst'].mean())):
@@ -115,19 +182,19 @@ timechunks = [("1854-01-01T00:00", "1870-01-12T00:00:00"),
 
 files_long = []
 download_path = '/Users/leonardobertini/Library/CloudStorage/OneDrive-SharedLibraries-UniversityofBristol/grp-Chapter 4 - Leo - General/Results/ERSSTv5'
-for pair in timechunks:
-    results = earthaccess.search_data(
-        short_name='REYNOLDS_NCDC_L4_MONTHLY_V5',
-        cloud_hosted=True,
-        bounding_box=(BBOX['SW_lon'],
-                      BBOX['SW_lat'],
-                      BBOX['NE_lon'],
-                      BBOX['NE_lat']),
-        temporal=pair,
-        count=2000)
-
-    files = earthaccess.download(results, download_path)
-    files_long.append(files)
+# for pair in timechunks:
+#     results = earthaccess.search_data(
+#         short_name='REYNOLDS_NCDC_L4_MONTHLY_V5',
+#         cloud_hosted=True,
+#         bounding_box=(BBOX['SW_lon'],
+#                       BBOX['SW_lat'],
+#                       BBOX['NE_lon'],
+#                       BBOX['NE_lat']),
+#         temporal=pair,
+#         count=2000)
+#
+#     files = earthaccess.download(results, download_path)
+#     files_long.append(files)
 
 
 ##TODO reading ERSSTv5 data until 2007
@@ -152,12 +219,17 @@ COADS_fixed = xr.decode_cf(COADS_fixed)
 
 #TODO reading HadlSST data
 
+
+
 ##Indonesian samples
 # TODO read spreadsheet and feed data into function to get averages
 excel_path = '/Users/leonardobertini/Library/CloudStorage/OneDrive-SharedLibraries-UniversityofBristol/grp-Chapter 4 - Leo - General/Results/Tables_and_Regional_Sectors_Averages.xlsx'
 Dataframe_MyData = pd.read_excel(excel_path, sheet_name="Table1")
 # remove first row
 Dataframe_MyData = Dataframe_MyData.drop([0]).reset_index()
+
+
+#TODO find closest available gridcell where value is not NaN
 
 Dataframe_MyData = extract_ERSSTv5(ncfille=ERSSTv5, Dataframe=Dataframe_MyData, TimeRangeCol='MGA year range',
                                    LatVarName='Lat', LonVarName='Lon',
